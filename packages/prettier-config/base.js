55a66aef3e1fdfa4b694d18fcd04696da3ccf191
Jared Ledbetter <jared@carbondigital.us>
2026-09-11 10:33:35 -0400

    setup prettier config package

---
+++ packages/prettier-config/base.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none'
+};
