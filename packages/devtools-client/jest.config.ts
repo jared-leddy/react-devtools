78a8f9f8ff376460212c14d0c4c3fdf0c2ff03f9
Jared Ledbetter <jared@carbondigital.us>
2026-09-21 04:44:23 -0400

    feat(client): scaffold devtools client package

---
+++ packages/devtools-client/jest.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/main.tsx',
+        '!src/pages/**',
+        '!src/components/tree/**',
+        '!src/components/state/**'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'html', 'lcov'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    moduleNameMapper: {
+        '^@devtools/ui$': '<rootDir>/tests/uiMock.tsx',
+        '^@devtools/ui/style.css$': 'identity-obj-proxy',
+        '\\.(css)$': 'identity-obj-proxy'
+    },
+    rootDir: '.',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jsdom',
+    testRegex: 'tests/.*\\.test\\.tsx?$',
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
+    }
+};
+
+export default config;
---
+++ packages/devtools-client/vite.standalone.config.ts
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        emptyOutDir: true,
+        outDir: 'dist/standalone'
+    },
+    plugins: [react()]
+});
