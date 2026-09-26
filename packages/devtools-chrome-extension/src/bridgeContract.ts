10e5cf0e8ad00c2382c2d1eaaabfb44f292e1f87
Jared Ledbetter <jared@carbondigital.us>
2026-09-26 04:15:19 -0400

    test(extension): add bridge contract coverage

---
+++ packages/devtools-chrome-extension/src/bridgeContract.ts
@@ -0,0 +1,17 @@
+export {
+    EXTENSION_RPC_EVENT_KEY,
+    EXTENSION_RPC_MESSAGE_SOURCE,
+    createExtensionClientChannel,
+    createExtensionPortChannel,
+    createExtensionProxyChannel,
+    createExtensionServerChannel,
+    isExtensionRpcEnvelope,
+    type ChromeLike,
+    type ExtensionDisconnectHandler,
+    type ExtensionMessageHandler,
+    type ExtensionPortLike,
+    type ExtensionRpcEnvelope,
+    type ExtensionWindowLike,
+    type ExtensionWindowMessageEvent,
+    type ExtensionWindowMessageListener
+} from '@devtools/kit';
